1e257d648142ccbc53d7d46de7be18cae9bacf39
Mattes Groeger <info@mattes-groeger.de>
2013-10-20 14:55:38 +0200

    Recreate project with Xcode 4

---
+++ TravisExampleTests/TravisExampleTests.h
@@ -0,0 +1,13 @@
+//
+//  TravisExampleTests.h
+//  TravisExampleTests
+//
+//  Created by Mattes Groeger on 20.10.13.
+//  Copyright (c) 2013 Mattes Groeger. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface TravisExampleTests : SenTestCase
+
+@end
--- TravisExampleTests/TravisExampleTests.m
+++ TravisExampleTests/TravisExampleTests.m
@@ -6,29 +6,27 @@
 //  Copyright (c) 2013 Mattes Groeger. All rights reserved.
 //
 
-#import <XCTest/XCTest.h>
-
-@interface TravisExampleTests : XCTestCase
-
-@end
+#import "TravisExampleTests.h"
 
 @implementation TravisExampleTests
 
 - (void)setUp
 {
     [super setUp];
-    // Put setup code here. This method is called before the invocation of each test method in the class.
+    
+    // Set-up code here.
 }
 
 - (void)tearDown
 {
-    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    // Tear-down code here.
+    
     [super tearDown];
 }
 
 - (void)testExample
 {
-    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+    STFail(@"Unit tests are not implemented yet in TravisExampleTests");
 }
 
 @end
