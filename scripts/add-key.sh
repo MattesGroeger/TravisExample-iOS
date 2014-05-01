bfab381c5562438db34a8f2ba4f39eec5bf506a8
Mattes Groeger <info@mattes-groeger.de>
2014-05-01 19:43:58 +0200

    Update certificate to developer & adjust keychain

--- scripts/add-key.sh
+++ scripts/add-key.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 security create-keychain -p travis ios-build.keychain
+security default-keychain -s ios-build.keychain
+security unlock-keychain -p travis ios-build.keychain
+security set-keychain-settings -t 3600 -u iso-build.keychain
 security import ./scripts/certs/apple.cer -k ~/Library/Keychains/ios-build.keychain -T /usr/bin/codesign
 security import ./scripts/certs/dist.cer -k ~/Library/Keychains/ios-build.keychain -T /usr/bin/codesign
 security import ./scripts/certs/dist.p12 -k ~/Library/Keychains/ios-build.keychain -P $KEY_PASSWORD -T /usr/bin/codesign
